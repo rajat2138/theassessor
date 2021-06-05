44eb9f4c5500daf12c9dd2be75b3adfcb3261caf
Rajat <rajat@pop-os.localdomain>
2021-06-05 11:33:04 +0530

    Change in Port in app.js

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.use(express.static(__dirname + '/views/static'));
 
 require("./routes/oauth.js")(app);
 
-let port = process.env.port || 5000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
 app.listen(port,function() {
   console.log("Server initiated at port "+port);
 });
